1b9115bbd211ef3cc35e57042ed362d50a100e23
Brian Loyal <bloyal@gmail.com>
2014-07-13 15:00:49 -0500

    plot 3 done
    Completed plot 3

--- plot3.R
+++ plot3.R
@@ -3,6 +3,10 @@ data<-downloadHouseholdData()
 
 png("plot3.png", width=480, heigh=480)
 
-plot(data$DateTime, data$Global_active_power, type="l", xlab='', ylab="Global Active Power (kilowatts)")
+plot(data$DateTime, data$Sub_metering_1, col="black", type="l", xlab="", ylab="Energy sub metering")
+points(data$DateTime, data$Sub_metering_2, col="red", type="l")
+points(data$DateTime, data$Sub_metering_3, col="blue", type="l", xlab="", ylab="Energy sub metering")
+
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col= c("black", "red", "blue"))
 
 dev.off()
