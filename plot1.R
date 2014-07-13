c7a954a90f02a171c5b330f9a3ad3571c6c4a9a5
Brian Loyal <bloyal@gmail.com>
2014-07-13 14:10:39 -0500

    plot1
    Completed plot1

--- plot1.R
+++ plot1.R
@@ -1,2 +1,6 @@
 source("downloadData.R")
-householdData<-downloadHouseholdData()
+data<-downloadHouseholdData()
+
+png("plot1.png", width=480, heigh=480)
+hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
