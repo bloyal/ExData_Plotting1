1bc3f4ae4faa1955fe20bae45b912b71d9175d7e
Brian Loyal <bloyal@gmail.com>
2014-07-13 14:34:44 -0500

    Fixing issue with sub metering 3

--- downloadData.R
+++ downloadData.R
@@ -10,9 +10,9 @@ downloadHouseholdData <- function(){
         print("Loading file into data frame")
         householdPower <- read.table("household_power_consumption.txt", header=FALSE, sep=";", skip=66637, nrows=2880, na.strings="?")
         colnames(householdPower)<- c("Date","Time", "Global_active_power", "Global_reactive_power", "Voltage", 
-        "Global_intensity", "Sub_metering_1", "Sub_metering_2")
+        "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
         DateTime<- strptime(paste(householdPower$Date,householdPower$Time," "),"%d/%m/%Y %H:%M:%S")
         
-        householdPower<- cbind(DateTime, householdPower[,3:8])
+        householdPower<- cbind(DateTime, householdPower[,3:9])
 }
